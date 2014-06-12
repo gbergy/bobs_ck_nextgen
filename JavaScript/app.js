8f12592ede0f1a8420d0d778e068ccd258a63fde
gbergy <greg.goldberg718@gmail.com>
2014-06-12 10:27:10 -0400

    Experimenting

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -5,23 +5,24 @@ $("#menu").append($select);
 //Cycle over menu links
 $("#menu a").each(function(){
   var $anchor = $(this);
-  //Create an option
+//Create an option
   var $option = $("<option></option>");
 
-  //Deal with selected options depending on current page
+//Deal with selected options depending on current page
   if($anchor.parent().hasClass("selected")) {
     $option.prop("selected", true);
   }
-  //option's value is the href
+//option's value is the href
   $option.val($anchor.attr("href"));
-  //option's text is the text of link
+//option's text is the text of link
   $option.text($anchor.text());
-  //append option to select
+//append option to select
   $select.append($option);
 });
 
-//Create and Bind change to select
+// //Create and Bind change to select
 $select.change(function(){
-  //Go to select's location
+//   //Go to select's location
   window.location = $select.val();
 });
+
